6839e8e315ecfd3794aa5f0de212f70ba937d03d
roshan-udyavara <roshanudyavara@gmail.com>
2023-08-28 13:20:13 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/roshan-udyavara/puppy
 
 go 1.20
+
+require github.com/roshan-udyavara/dog v0.0.0-20230828073906-931265abfa75 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/roshan-udyavara/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barks())
 }
